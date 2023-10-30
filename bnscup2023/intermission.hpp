0e8e4da0ff7ad2bce4188e2bed3d9d3f87cddf65
tabei <74161279+Bet-Amoto@users.noreply.github.com>
2023-10-31 01:10:26 +0900

    ゲーム切り替えの基礎の基礎

---
+++ bnscup2023/intermission.hpp
@@ -0,0 +1,10 @@
+﻿#pragma once
+#include "Common.hpp"
+#include "PetitGame.hpp"
+
+class InterMisson
+{
+public:
+	virtual void update();
+	virtual void draw(const RenderTexture& gamescene,double fade) const;
+};
