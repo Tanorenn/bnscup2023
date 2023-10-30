dcf93cd2ffc7d0113032ed9c8321febf89e703ce
BetAmoto <tabebetomo@gmail.com>
2023-10-30 16:23:50 +0900

    iroiro

---
+++ bnscup2023/Sukuea.hpp
@@ -0,0 +1,15 @@
+﻿#pragma once
+# include "PetitGame.hpp"
+
+class Sukuea : public PetitGame
+{
+public:
+	Sukuea();
+	void init() override;
+	void update(double t, double gameSpeed) override;
+	void draw() const override;
+	bool isClear() override;
+
+private:
+
+};
